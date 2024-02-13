5d1f090577dc6b8464551711e66961558b3efe4f
W7 Java <W7 Java@W7JAVA-PC>
2024-02-13 08:51:00 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicios_7_2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios_7_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
